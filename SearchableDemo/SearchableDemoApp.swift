ecd7891e084b464114521fc8a90a64c2304c65c3
Davide Troise <davide@troise.net>
2022-12-26 22:02:56 +0100

    Initial Commit

---
+++ SearchableDemo/SearchableDemoApp.swift
@@ -0,0 +1,18 @@
+//
+//  SearchableDemoApp.swift
+//  SearchableDemo
+//
+//  Created by Davide Troise with Swift 5.0
+//  for the YouTube channel "Coding con Davide" https://bit.ly/3QJziJE
+//
+
+import SwiftUI
+
+@main
+struct SearchableDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
